018b12f8ea597a123966e4139d447aaf9cf7595b
241522 <241522@student.pwr.edu.pl>
2022-01-06 13:23:37 +0100

    load multiple images, error handling, checkboxes for algorithms

--- UI/src/components/ImageDrop.js
+++ UI/src/components/ImageDrop.js
@@ -4,9 +4,44 @@ import Drop from "./images/drop.svg";
 const fs = window.require("fs");
 // const pathModule = window.require("path");
 
+const imageTypes = [
+  "image/png",
+  "image/jpeg",
+  "image/ico",
+  "image/tiff",
+  "image/gif",
+];
+
 function ImageDrop({ loaded, setLoaded, path, setPath }) {
   const [image, setImage] = useState();
   const [imageDispl, setImageDispl] = useState();
+  const [errMsg, setErrMsg] = useState();
+
+  const onDropEvent = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    try {
+      let filesObj = e.dataTransfer.files;
+      let files = Object.keys(filesObj).map((key) => filesObj[key]);
+      let fileTypes = files.map((file) => file.type);
+      fileTypes.forEach((fileType) => {
+        let tmp = imageTypes.some((type) => type === fileType);
+        console.log(tmp);
+        if (!tmp) throw "Wrong file type!";
+      });
+      setImage(files);
+      setPath(files.map((file) => file.path));
+      let _img = files.map((file) =>
+        fs.readFileSync(file.path).toString("base64")
+      );
+      setImageDispl(_img);
+      setErrMsg(null);
+      setLoaded(true);
+    } catch (er) {
+      setErrMsg(er);
+      setLoaded(false);
+    }
+  };
 
   return (
     <div
@@ -14,6 +49,7 @@ function ImageDrop({ loaded, setLoaded, path, setPath }) {
       style={{
         width: loaded && "50%",
         height: loaded ? "fit-content" : "100%",
+        maxHeight: "100%",
         borderStyle: loaded && "solid",
       }}
       onDragOver={(e) => {
@@ -21,30 +57,27 @@ function ImageDrop({ loaded, setLoaded, path, setPath }) {
         e.preventDefault();
       }}
       onDrop={(e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        try {
-          setImage(e.dataTransfer.files[0]);
-          setPath(e.dataTransfer.files[0].path);
-          let _img = fs
-            .readFileSync(e.dataTransfer.files[0].path)
-            .toString("base64");
-          setImageDispl(_img);
-          setLoaded(true);
-        } catch (er) {
-          console.log(er);
-          setLoaded(false);
-        }
+        onDropEvent(e);
       }}
     >
       {loaded ? (
-        <img
-          src={`data:${image.type};base64,` + imageDispl}
-          style={{ width: "calc(50vw - 30px)", objectFit: "contain" }}
-          alt=""
-        />
+        <div className="images-container">
+          {imageDispl?.map((filePath, key) => (
+            <img
+              key={key}
+              src={`data:${image[key].type};base64,` + filePath}
+              style={{
+                width: image.length > 1 ? "100%" : "calc(50vw - 30px)",
+              }}
+              alt=""
+            />
+          ))}
+        </div>
       ) : (
-        <img src={Drop} alt="" />
+        <div>
+          <img src={Drop} alt="" />
+          <div className="error">{errMsg}</div>
+        </div>
       )}
     </div>
   );
--- UI/src/components/Options.js
+++ UI/src/components/Options.js
@@ -1,30 +1,62 @@
+import { useState } from "react";
+
 import axios from "axios";
 
 function Options({ path, setDataPresent, setData }) {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [checks, setChecks] = useState({ deep: false, feature: false });
+
   const sendPath = async () => {
-    try {
-      let tmp = await axios.post(
-        "http://127.0.0.1:5000/image",
-        {
-          path: path,
-        },
-        { headers: { "content-type": "application/json" } }
-      );
-      console.log(tmp);
-      if (tmp.data) {
-        setDataPresent(true);
-        setData(tmp.data);
+    if (checks.feature || checks.deep) {
+      setLoading(true);
+      try {
+        let tmp = await axios.post(
+          "http://127.0.0.1:5000/image",
+          {
+            algorithms: checks,
+            path: path,
+          },
+          { headers: { "content-type": "application/json" } }
+        );
+        if (tmp?.data) {
+          setError(false);
+          setDataPresent(true);
+          setData(tmp.data);
+        }
+      } catch (er) {
+        setError(true);
       }
-    } catch (er) {
-      console.log(er);
+      setLoading(false);
     }
   };
   return (
     <div className="options">
-      <div className="choices">Deep Learning</div>
-      <div className="proc-button center" onClick={() => sendPath()}>
-        Process
+      <div className="choices">
+        <label class="check-container">
+          Deep Learning
+          <input
+            type="checkbox"
+            checked={checks.deep}
+            onChange={() => setChecks({ ...checks, deep: !checks.deep })}
+          />
+          <span class="checkmark"></span>
+        </label>
+
+        <label class="check-container">
+          Feature Based
+          <input
+            type="checkbox"
+            checked={checks.feature}
+            onChange={() => setChecks({ ...checks, feature: !checks.feature })}
+          />
+          <span class="checkmark"></span>
+        </label>
       </div>
+      <div className="proc-button center" onClick={() => sendPath()}>
+        {loading ? <div className="loading" /> : "Process"}
+      </div>
+      {error && <div className="error">Connection Error</div>}
     </div>
   );
 }
